48bb6ca41354d547de8ceaabcd2735c55dd78bfa
Aashman Jeremias Dcosta <90471468+Aashbot@users.noreply.github.com>
2024-05-31 12:48:48 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        lightBlue: '#e0f7fa',
+        darkBlue: '#00796b',
+        lightGreen: '#e8f5e9',
+        darkGreen: '#2e7d32',
+        lightYellow: '#fffde7',
+        darkYellow: '#fbc02d',
+        lightPink: '#fce4ec',
+        darkPink: '#d81b60',
+        lightGray: '#f5f5f5',
+        darkGray: '#bdbdbd',
+      },
+    },
+  },
+  plugins: [],
+}
+
